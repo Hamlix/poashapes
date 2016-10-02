469a01bd45f8da729f8b3c19ebcb419df2da3976
Hamlix <nathanaelle@famille-viller.com>
2016-10-02 18:05:24 +0200

    Ajout de Translatable2D et des modifications liés

--- src/Shape2D.java
+++ src/Shape2D.java
@@ -1,7 +1,7 @@
 /**
  * Created by Hamlix on 02/10/2016.
  */
-public abstract class Shape2D extends Shape {
+public abstract class Shape2D extends Shape implements Translatable2D{
 
     Point2D refPoint;
 
